7d9d89cad9fe56803cd2e6c9acc5060e71d7b1c1
liuyu <13681191863@163.com>
2017-06-15 12:40:57 +0800

    账户余额页面

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,6 +1,20 @@
 <template>
-	<div> 
-		这里是账户余额
+	<div id="content"> 
+		<h1>账户余额</h1>
+		<div class="accountTop">
+			<p>￥:0.00</p>
+			<button>充值</button>
+		</div>
+		<div class="accountmoney">
+			<a href="">充值记录</a>
+			<a href="">遇到问题</a>
+		</div>
+		<div class="accountCenter">
+			<p>￥:0.00</p>
+			<p class="copy">由于苹果公司政策限制，豆币仅能在「豆瓣阅读 iOS App」内充值及使用。</p>
+		</div>
+		
+		<p><a href="">消费记录</a></p>
 	</div>
 </template>
 
@@ -16,5 +30,56 @@
 </script>
 
 <style scoped>
-	
+	#content{
+		background:lavender;
+		padding:0 0.3rem;
+	}
+	h1{
+		font-size: 0.3rem;
+		text-align: center;
+		color:#aac6d0;
+		font-weight: 800;
+		height:1rem;
+		line-height: 1rem;
+	}
+	.accountTop,.accountCenter{
+		text-align: center;
+		background:#fff;
+		border-radius: 5px;
+		height:25vh;
+		border-bottom: 1px solid gainsboro;
+	}
+	p{
+		font-size: 0.5rem;
+		color:#6a9acc;
+		margin-top:0.3rem;
+	}
+	button{
+		width:80vw;
+		height:0.8rem;
+		border-radius:0.4rem;
+		background:#77a4b3;
+		font-size: 0.3rem;
+		margin-top:-0.8rem;
+		color:#fff;
+	}
+	.accountmoney{
+		height:0.8rem;
+		font-size: 0.3rem;
+    	text-align: center;
+    	line-height: 0.8rem;
+    	background:#fff;
+	}
+	.accountmoney a{
+		width:35vw;
+		padding:0 0.4rem;
+	}
+	.accountCenter{
+		margin-top:0.5rem;
+	}
+	.copy{
+		font-size: 0.25rem;
+		color:#a6a6a6;
+		padding:0 0.2rem;
+	}
 </style>
